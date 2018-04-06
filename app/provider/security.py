9bf62186187aa5152cbd9ef952bf551db20d390a
yntonfon <ynguieye@gmail.com>
2018-04-06 04:00:46 +0200

    feat(securityProvider): add url_safe_timed_serializer

--- app/provider/security.py
+++ app/provider/security.py
@@ -1,15 +1,21 @@
 from flask import current_app
+from itsdangerous import URLSafeTimedSerializer
 
 from app.extension import bcrypt
 
 
 class SecurityProvider:
-    def __init__(self, app, bcrypt):
+    def __init__(self, app, bcrypt, urlsafetimedserializer_cls):
         self.app = app
         self.bcrypt = bcrypt
+        self.urlsafetimedserializer_cls = urlsafetimedserializer_cls
     
     def encrypt_password(self, password):
         return self.bcrypt.generate_password_hash(password, self.app.config['BCRYPT_LOG_ROUNDS'])
 
+    def build_url_safe_timed_token(self, token, salt):
+        serializer = self.urlsafetimedserializer_cls(self.app.config['SECRET_KEY'])
+        return serializer.dumps(token, salt=salt)
+        
 
-security_provider = SecurityProvider(current_app, bcrypt)
+security_provider = SecurityProvider(current_app, bcrypt, URLSafeTimedSerializer)
--- tests/app/provider/test_securityProvider.py
+++ tests/app/provider/test_securityProvider.py
@@ -1,7 +1,9 @@
 from unittest import TestCase, mock
+from unittest.mock import Mock
 
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from itsdangerous import URLSafeTimedSerializer
 
 from app.provider.security import SecurityProvider
 
@@ -10,8 +12,9 @@ class TestSecurityProvider(TestCase):
     def setUp(self):
         self.mock_bcrypt = mock.create_autospec(Bcrypt)
         self.mock_app = mock.create_autospec(Flask)
+        self.mock_url_safe_timed_serializer = mock.create_autospec(URLSafeTimedSerializer)
         
-        self.security_provider = SecurityProvider(self.mock_app, self.mock_bcrypt)
+        self.security_provider = SecurityProvider(self.mock_app, self.mock_bcrypt, self.mock_url_safe_timed_serializer)
         
     def test_encrypt_password_generates_hash_of_the_given_password(self):
         # Given
@@ -23,3 +26,27 @@ class TestSecurityProvider(TestCase):
         
         # Then
         self.mock_bcrypt.generate_password_hash.assert_called_with(password, 12)
+
+    def test_build_url_safe_timed_token_instanciates_serializer_class_with_secret_key(self):
+        # Given
+        token = 'stringtosafe'
+        self.mock_app.config = {'SECRET_KEY': 'uhooh'}
+        
+        # When
+        self.security_provider.build_url_safe_timed_token(token, salt='salt')
+        
+        # Then
+        self.mock_url_safe_timed_serializer.assert_called_with('uhooh')
+
+    def test_build_url_safe_timed_token_generates_URL_safe_string_with_time_information(self):
+        # Given
+        token = 'stringtosafe'
+        serialize = Mock()
+        self.mock_app.config = {'SECRET_KEY': 'uhooh'}
+        self.mock_url_safe_timed_serializer.return_value = serialize
+        
+        # When
+        self.security_provider.build_url_safe_timed_token(token, salt='salt')
+        
+        # Then
+        serialize.dumps.assert_called_with(token, salt='salt')
