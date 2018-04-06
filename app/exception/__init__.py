6cd4d0df92e734081270771b102ba653187428e6
yntonfon <ynguieye@gmail.com>
2018-04-06 23:35:24 +0200

    feat(exception): add new exceptions class

--- app/exception/__init__.py
+++ app/exception/__init__.py
@@ -1,2 +1,2 @@
-from .user import UserAlreadyExistException
+from .user import UserAlreadyExistException, UserInvalidTokenException, UserNotFoundException
 from .views import ViewsException
--- app/exception/user.py
+++ app/exception/user.py
@@ -2,5 +2,17 @@ class UserAlreadyExistException(Exception):
     def __init__(self):
         self.messages = {
             'error_code': 'user-already-exist',
-            'description': 'The user seems to be already created. Choose another username or email'
+            'description': 'The user seems to be already created. Choose another username or email.'
         }
+
+
+class UserInvalidTokenException(Exception):
+    def __init__(self):
+        self.messages = {
+            'error_code': 'user-invalid-token',
+            'description': 'The token seems to be incorrect. Please request a fresh one.'
+        }
+
+
+class UserNotFoundException(Exception):
+    pass
