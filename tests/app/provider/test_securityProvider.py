f89837f0a1dac0416a74d75298f3b13e2fc5bb04
yntonfon <ynguieye@gmail.com>
2018-04-06 18:34:34 +0200

    refactor(securityProvider): bind app via init_app function

--- tests/app/provider/test_securityProvider.py
+++ tests/app/provider/test_securityProvider.py
@@ -12,41 +12,54 @@ class TestSecurityProvider(TestCase):
     def setUp(self):
         self.mock_bcrypt = mock.create_autospec(Bcrypt)
         self.mock_app = mock.create_autospec(Flask)
-        self.mock_url_safe_timed_serializer = mock.create_autospec(URLSafeTimedSerializer)
+        self.mock_urlsafetimed_serializer = mock.create_autospec(URLSafeTimedSerializer)
+        self.provider = SecurityProvider(self.mock_bcrypt, self.mock_urlsafetimed_serializer)
         
-        self.security_provider = SecurityProvider(self.mock_app, self.mock_bcrypt, self.mock_url_safe_timed_serializer)
+        self.mock_app.config = {'SECRET_KEY': 'mysecret', 'BCRYPT_LOG_ROUNDS': 12}
         
+    def test_init_app_sets_app(self):
+        # When
+        self.provider.init_app(self.mock_app)
+        
+        # Then
+        self.assertEqual(self.mock_app, self.provider.app)
+        
+    def test_init_app_instanciates_new_urlsafetimed_serializer(self):
+        # When
+        self.provider.init_app(self.mock_app)
+        
+        # Then
+        self.mock_urlsafetimed_serializer.assert_called_with('mysecret')
+        
+    def test_init_app_sets_new_urlsafetimed_serializer(self):
+        # Given
+        self.mock_urlsafetimed_serializer.return_value = 'serializer'
+        
+        # When
+        self.provider.init_app(self.mock_app)
+        
+        # Then
+        self.assertEqual('serializer', self.provider.urlsafetimed_serializer)
+    
     def test_encrypt_password_generates_hash_of_the_given_password(self):
         # Given
         password = 'mysecret'
-        self.mock_app.config = {'BCRYPT_LOG_ROUNDS': 12}
+        self.provider.app = self.mock_app
         
         # When
-        self.security_provider.encrypt_password(password)
+        self.provider.encrypt_password(password)
         
         # Then
         self.mock_bcrypt.generate_password_hash.assert_called_with(password, 12)
 
-    def test_build_url_safe_timed_instanciates_serializer_class_with_secret_key(self):
-        # Given
-        token = 'stringtosafe'
-        self.mock_app.config = {'SECRET_KEY': 'uhooh'}
-        
-        # When
-        self.security_provider.build_url_safe_timed(token, salt='salt')
-        
-        # Then
-        self.mock_url_safe_timed_serializer.assert_called_with('uhooh')
-
     def test_build_url_safe_timed_generates_URL_safe_string_with_time_information(self):
         # Given
-        token = 'stringtosafe'
-        serialize = Mock()
-        self.mock_app.config = {'SECRET_KEY': 'uhooh'}
-        self.mock_url_safe_timed_serializer.return_value = serialize
+        data = 'datatosecure'
+        salt = 'mysalt'
+        self.provider.urlsafetimed_serializer = self.mock_urlsafetimed_serializer
         
         # When
-        self.security_provider.build_url_safe_timed(token, salt='salt')
+        self.provider.build_url_safe_timed(data, salt=salt)
         
         # Then
-        serialize.dumps.assert_called_with(token, salt='salt')
+        self.mock_urlsafetimed_serializer.dumps.assert_called_with(data, salt=salt)
