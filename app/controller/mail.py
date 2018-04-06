47771fec050dbcb5da920a4bcb48601da578eb4c
yntonfon <ynguieye@gmail.com>
2018-04-06 17:49:11 +0200

    typo(mail): fix PEP8

--- app/controller/mail.py
+++ app/controller/mail.py
@@ -10,11 +10,11 @@ class MailController:
     
     def send_confirmation_email_link(self, email):
         token = self.security_provider.build_url_safe_timed(email, salt=security_provider.EMAIL_CONFIRMATION_LINK_KEY)
-        url =self.url_provider.build_url_from(url_provider.USER_CONFIRM_EMAIL_API, external=True, token=token)
+        url = self.url_provider.build_url_from(url_provider.USER_CONFIRM_EMAIL_API, external=True, token=token)
         body = self.template_provider.render_template(template_provider.EMAIL_ACTIVATE_TPL, confirm_url=url)
         
         msg = MsgTemplate('Confirm your email', html=body, recipients=[email])
         return self.mail_provider.send(msg)
-        
+
 
 mail_controller = MailController(security_provider, url_provider, template_provider, mail_provider)
