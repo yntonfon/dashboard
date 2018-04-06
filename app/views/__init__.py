988cf9899b2a6373b0ab9cbac4b8dcfb49a07112
yntonfon <ynguieye@gmail.com>
2018-04-06 09:15:06 +0200

    feat(userViews) move user blueprint to __init__

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,7 +1,10 @@
+from flask import Blueprint
+
 from app.exception import ViewsException
-from .user_list_api import user_blueprint, UserListAPI
+from .user_list_api import UserListAPI
 from .utils import handle_views_exception
 
 # Users API
+user_blueprint = Blueprint('user', __name__)
 user_blueprint.add_url_rule('/users', view_func=UserListAPI.as_view('users'))
 user_blueprint.register_error_handler(ViewsException, handle_views_exception)
---
+++ app/views/user_confirmation_email_api.py
@@ -0,0 +1,6 @@
+from flask.views import MethodView
+
+
+class UserConfirmationEmailAPI(MethodView):
+    def get(self):
+        pass
