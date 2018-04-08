a2b6904eb5afc2680e131b5dde255be229b6d0f1
yntonfon <ynguieye@gmail.com>
2018-04-08 04:29:15 +0200

    feat(user): implement get logic

--- app/views/user_reset_password_api.py
+++ app/views/user_reset_password_api.py
@@ -1,11 +1,22 @@
-from flask import request, jsonify, abort
+from flask import request, jsonify, abort, make_response
 from flask.views import MethodView
 
 from app.controller import user_controller, mail_controller
-from app.exception import UserNotFoundException, UserNotActiveException, ViewsException
+from app.exception import UserNotFoundException, UserNotActiveException, ViewsException, UserInvalidTokenException
 
 
 class UserResetPasswordAPI(MethodView):
+    def get(self):
+        url_params = request.args
+        
+        try:
+            result = user_controller.reset_password(url_params['token'])
+        except UserInvalidTokenException:
+            abort(404)
+        else:
+            mail_controller.send_new_password(**result)
+            return make_response(jsonify(), 201)
+    
     def put(self):
         payload = request.get_json()
         email = payload['email']
@@ -17,6 +28,6 @@ class UserResetPasswordAPI(MethodView):
             abort(404)
         except UserNotActiveException as e:
             raise ViewsException(status_code=400, payload=e.messages)
-    
-        mail_controller.send_reset_password_link(email)
-        return jsonify()
+        else:
+            mail_controller.send_reset_password_link(email)
+            return jsonify()
--- tests/app/views/test_userResetPasswordAPI.py
+++ tests/app/views/test_userResetPasswordAPI.py
@@ -1,16 +1,67 @@
 import json
 
 from app.model import User
+from app.provider import security_provider, SaltEnum
 from tests.base_test import BaseTest
 
 
 class TestUserResetPasswordAPI(BaseTest):
+    def test_get_resets_user_password_and_send_the_new_password_by_email(self):
+        # Given
+        user = User(username='Robert', email='robert@test.com', password_hash='secrethashed')
+        self.db.session.add(user)
+        self.db.session.commit()
+        
+        token = security_provider.encrypt_to_urlsafetimed('robert@test.com', salt=SaltEnum.reset_password.value)
+        
+        # When
+        with self.mail.record_messages() as outbox:
+            response = self.client.get('/api/user/reset?token=%s' % token)
+        
+        # Then
+        self.assertStatus(response, 201)
+        self.assertEqual(1, len(outbox))
+        self.assertIn('robert@test.com', outbox[0].recipients)
+        
+        self.db.session.refresh(user)
+        self.assertNotEqual('secrethashed', user.password_hash)
+    
+    def test_get_returns_404_when_user_send_an_invalid_token(self):
+        # Given
+        user = User(username='Robert', email='robert@test.com', password_hash='secrethashed')
+        self.db.session.add(user)
+        self.db.session.commit()
+        
+        bad_token = security_provider.encrypt_to_urlsafetimed('robert@test.com', salt='bad-salt')
+        
+        # When
+        with self.mail.record_messages() as outbox:
+            response = self.client.get('/api/user/reset?token=%s' % bad_token)
+        
+        # Then
+        self.assertStatus(response, 404)
+    
+    def test_get_returns_404_when_user_is_not_found(self):
+        # Given
+        user = User(username='Robert', email='robert@test.com', password_hash='secrethashed')
+        self.db.session.add(user)
+        self.db.session.commit()
+        
+        bad_token = security_provider.encrypt_to_urlsafetimed('INVALID@test.com', salt=SaltEnum.reset_password.value)
+        
+        # When
+        with self.mail.record_messages() as outbox:
+            response = self.client.get('/api/user/reset?token=%s' % bad_token)
+        
+        # Then
+        self.assertStatus(response, 404)
+    
     def test_put_sends_an_email_with_link_password_recovery_to_user(self):
+        # Given
         user = User(username='Robert', email='robert@test.com', password_hash='secrethashed', email_confirmed=True)
         self.db.session.add(user)
         self.db.session.commit()
         
-        # Given
         data = json.dumps({'email': 'robert@test.com'})
         
         # When
