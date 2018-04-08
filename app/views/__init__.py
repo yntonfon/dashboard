cd41133fc75c85e7f74d5d20b98dcc5c7df9488f
yntonfon <ynguieye@gmail.com>
2018-04-08 02:16:59 +0200

    feat(user): implement reset logic API

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -10,5 +10,5 @@ from .utils import handle_views_exception
 user_blueprint = Blueprint('user', __name__)
 user_blueprint.add_url_rule('/users', view_func=UserListAPI.as_view('users'))
 user_blueprint.add_url_rule('/user/confirm/<string:token>', view_func=UserConfirmEmailAPI.as_view('user_confirm'))
-user_blueprint.add_url_rule('/user/reset', view_func=UserResetPasswordAPI.as_view('user.reset'))
+user_blueprint.add_url_rule('/user/reset', view_func=UserResetPasswordAPI.as_view('user_reset'))
 user_blueprint.register_error_handler(ViewsException, handle_views_exception)
--- app/views/user_reset_password_api.py
+++ app/views/user_reset_password_api.py
@@ -1,6 +1,19 @@
+from flask import request, jsonify, abort
 from flask.views import MethodView
+
+from app.controller import user_controller, mail_controller
+from app.exception import UserNotFoundException
 
 
 class UserResetPasswordAPI(MethodView):
     def put(self):
-        pass
+        payload = request.get_json()
+        email = payload['email']
+    
+        try:
+            user_controller.get_user(email)
+        except UserNotFoundException:
+            abort(404)
+    
+        mail_controller.send_reset_password_link(email)
+        return jsonify()
